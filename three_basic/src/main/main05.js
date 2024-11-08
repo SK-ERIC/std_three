c9007e3b2dd1792d07f119943dbd4db0e759e4b4
SK-ERIC <shikang@cloudcsp.com>
2024-11-08 08:22:57 +0800

    requestAnimationFrame 时间参数 控制物体动画

---
+++ three_basic/src/main/main05.js
@@ -0,0 +1,39 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 0, 10);
+scene.add(camera);
+
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+cube.rotation.set(Math.PI / 4, 0, 0, "XYZ");
+
+scene.add(cube);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+function render(time) {
+  let t = (time / 1000) % 5;
+  cube.position.x = t * 1;
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+render();

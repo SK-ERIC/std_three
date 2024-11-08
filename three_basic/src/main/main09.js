67903d862112628450be1085f961e7375d650109
SK-ERIC <shikang@cloudcsp.com>
2024-11-08 08:37:02 +0800

    js控制画面全屏

---
+++ three_basic/src/main/main09.js
@@ -0,0 +1,89 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import gsap from "gsap";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 0, 10);
+scene.add(camera);
+
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+cube.rotation.set(Math.PI / 4, 0, 0, "XYZ");
+
+scene.add(cube);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+let animate1 = gsap.to(cube.position, {
+  x: 5,
+  duration: 5,
+  ease: "power1.inOut",
+  repeat: -1,
+  yoyo: true,
+  delay: 2,
+  onComplete: () => {
+    console.log("动画完成");
+  },
+  onStart: () => {
+    console.log("动画开始");
+  },
+});
+gsap.to(cube.rotation, { x: 2 * Math.PI, duration: 5, ease: "power1.inOut" });
+
+window.addEventListener("dblclick", () => {
+  if (animate1.isActive()) {
+    animate1.pause();
+  } else {
+    animate1.resume();
+  }
+});
+
+window.addEventListener("dblclick", () => {
+  const fullScreenElement = document.fullscreenElement;
+  if (!fullScreenElement) {
+    //   双击控制屏幕进入全屏，退出全屏
+    // 让画布对象全屏
+    renderer.domElement.requestFullscreen();
+  } else {
+    //   退出全屏，使用document对象
+    document.exitFullscreen();
+  }
+  //   console.log(fullScreenElement);
+});
+
+function render() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+render();
+
+// 监听画面变化，更新渲染画面
+window.addEventListener("resize", () => {
+  //   console.log("画面变化了");
+  // 更新摄像头
+  camera.aspect = window.innerWidth / window.innerHeight;
+  //   更新摄像机的投影矩阵
+  camera.updateProjectionMatrix();
+
+  //   更新渲染器
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  //   设置渲染器的像素比
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
